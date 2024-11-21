9d8a1e3cadf11154ec8837f82d399e0856eac95c
UthiraS <sivaramanuthiralakshmi@gmail.com>
2024-11-20 19:56:03 -0500

    Fixed Skills Percentage Experience title

---
+++ images/'Kanguva' movie review_ A thousand-year-old spectacle with only a few seconds of soul_files/cfg.php
@@ -0,0 +1,1 @@
+try{__afflib.hbGetCnf({"gdpr":false,"ccpa":false,"geo":{"v":"2","cn":"north_america","cc":"us","st":"-","ip4":"24.151.66.15"},"dc":"gcp_iowa"})}catch(e){}
---
+++ images/'Kanguva' movie review_ A thousand-year-old spectacle with only a few seconds of soul_files/container_data/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_1731796194299614({ImpressionID:"27abea9290cd4de386987de3445eb768", ServerPublicDns:"rtbc-ue1.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "27abea9290cd4de386987de3445eb768");}catch(e){}
+try{__verify_callback_1731796194299614({
+ResultID:2,
+Passback:"",
+AdWidth:970,
+AdHeight:90});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "27abea9290cd4de386987de3445eb768");}catch(e){}
